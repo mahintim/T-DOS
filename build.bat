b90dd84246e7e07d567e2d28bc4f8efff1f532e9
timmahin <mahin.tim@gmail.com>
2019-11-05 10:10:22 -0600

    Added build script for Windows

--- build.bat
+++ build.bat
@@ -9,9 +9,9 @@ nasm BOOT\MAIN.S -o BIN\BOOT.B
 ndisasm BIN\BOOT.B -b16 -o0x7C08 > DISASM\BOOT.S
 python3 py\mkfs_tfs.py BIN\DISK.B BIN\BOOT.B 2048 1
 
-nasm KERNEL/MAIN.S -Xgnu -o MOUNT/KERNEL.X
-ndisasm MOUNT/KERNEL.X -b16 -o0x0800 > DISASM/KERNEL.S
+nasm KERNEL\MAIN.S -Xgnu -o MOUNT\KERNEL.X
+ndisasm MOUNT\KERNEL.X -b16 -o0x0800 > DISASM\KERNEL.S
 
-python3 py/mkfs_tfs.py BIN/DISK.B BIN/BOOT.B 2048 1
+python3 py\mnt_tfs.py -d BIN\DISK.B MOUNT
 
-qemu-system-i386.exe BIN/DISK.B
+qemu-system-i386.exe BIN\DISK.B
