172fe6e7d3d4f907a592798979f67eda4d586703
timmahin <mahin.tim@gmail.com>
2019-11-06 15:12:47 -0600

    Kernel's entry point changed to $900 everywhere

--- build.bat
+++ build.bat
@@ -10,7 +10,7 @@ ndisasm BIN\BOOT.B -b16 -o0x7C08 > DISASM\BOOT.S
 python3 py\mkfs_tfs.py BIN\DISK.B BIN\BOOT.B 2048 1
 
 nasm KERNEL\MAIN.S -Xgnu -o MOUNT\KERNEL.X
-ndisasm MOUNT\KERNEL.X -b16 -o0x0800 > DISASM\KERNEL.S
+ndisasm MOUNT\KERNEL.X -b16 -o0x0900 > DISASM\KERNEL.S
 
 python3 py\mnt_tfs.py -d BIN\DISK.B MOUNT
 
